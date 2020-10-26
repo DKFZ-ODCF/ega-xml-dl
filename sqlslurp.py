64d14ad7ebce92430c4453bc378c385bf2148320
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2020-10-26 20:12:02 +0100

    add experiment parsing: tested, sql-JOIN works on `text`, hurray!

--- sqlslurp.py
+++ sqlslurp.py
@@ -24,15 +24,25 @@ def get_ega_xml_dir(which_box):
 def create_or_open_db(db_file):
   conn = sqlite3.connect(db_file)
 
-  conn.executescript(
-  """DROP TABLE IF EXISTS studies;
+  conn.executescript("""
+    DROP TABLE IF EXISTS studies;
     CREATE TABLE studies (
-	EGAS varchar(15) PRIMARY KEY,
-	ERP varchar(10),
-	PMID integer,
-	title text NOT NULL,
+	EGAS        varchar(15) PRIMARY KEY,
+	ERP         varchar(10),
+	PMID        integer,
+	title       text NOT NULL,
 	description text NOT NULL
-      );"""
+    );
+
+    DROP TABLE IF EXISTS experiments;
+    CREATE TABLE experiments (
+        EGAX      varchar(15) PRIMARY KEY,
+        ERX       varchar(10),
+        XREF_ERP  varchar(10) NOT NULL,
+        XREF_EGAS varchar(15),
+        XREF_ERS  varchar(10) NOT NULL
+    );
+  """
   )
 
   return conn
@@ -86,6 +96,50 @@ def process_studies(db_conn, box_dir):
   log.info("finished study parsing")
 
 
+def extract_exp_info(path):
+  """Extracts the 'interesting' elements from an EGA experiment XML representation.
+
+  Parameter path must be a pathlib Path to an experiment XML file.
+
+  Returns a tuple of parsed fields, suitable for direct ingestion into the DB:
+  (egax, erx, xref_study_erp, xref_study_egas, xref_sample_ers)
+  """
+
+  log.debug("processing file %s", path)
+
+  # extract EGAX-number from filename, since it appears nowhere in the XML.
+  # XML internally uses ENA-style ERX
+  egax_number = path.name
+
+  xml = ET.parse(path)
+
+  # ENA ERX number
+  erx_number = xml.find("./EXPERIMENT/IDENTIFIERS/PRIMARY_ID").text
+
+  xref_study_erp = xml.find("./EXPERIMENT/STUDY_REF/IDENTIFIERS/PRIMARY_ID").text
+
+  xref_study_egas = xml.find("./EXPERIMENT/STUDY_REF/IDENTIFIERS/SECONDARY_ID")
+  if xref_study_egas != None:
+    xref_study_egas = xref_study_egas.text
+
+  xref_sample_ers = xml.find("./EXPERIMENT/DESIGN/SAMPLE_DESCRIPTOR/IDENTIFIERS/PRIMARY_ID").text
+
+  result = (egax_number, erx_number, xref_study_erp, xref_study_egas, xref_sample_ers )
+  log.debug("  result: %s", result)
+  return result
+
+
+def process_experiments(db_conn, box_dir):
+  exp_dir = box_dir / 'experiments'
+
+  exp_files = exp_dir.glob('EGAX*')
+  parsed_exps = ( extract_exp_info(f) for f in exp_files )
+  insert_exp_sql = "INSERT INTO experiments VALUES (?, ?, ?, ?, ?);"
+  db_conn.executemany(insert_exp_sql, parsed_exps);
+
+  log.info("finished experiment parsing")
+
+
 def main():
   log.basicConfig( level=log.DEBUG, format='%(levelname)s: %(message)s' )
 
@@ -97,6 +151,7 @@ def main():
   log.info("slurping XMLs from %s into %s", box_dir, db_file)
 
   process_studies(db_conn, box_dir)
+  process_experiments(db_conn, box_dir)
 
   db_conn.commit()
   db_conn.close()
