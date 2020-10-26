6c273900c781bd476016f05218b784a3fcc4f8cc
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2020-10-26 17:40:25 +0100

    first bits of SQLite handling.

--- sqlslurp.py
+++ sqlslurp.py
@@ -2,6 +2,7 @@
 
 import logging as log
 import sys
+import sqlite3
 from pathlib import Path
 import xml.etree.ElementTree as ET
 
@@ -9,6 +10,8 @@ import xml.etree.ElementTree as ET
 # Hardcoded basedir to match the default output location from the ega-xml-dl shell-script
 basedir = "~/ega-xml/"
 
+# filename for the sqlite result that will be written in the box dir
+dbName = 'box-contents.sqlite'
 
 def getEgaXmlDir(whichBox):
   """Returns the canonical path to the cache for `whichBox`.
@@ -18,8 +21,23 @@ def getEgaXmlDir(whichBox):
   return Path.home() / 'ega-xml' / whichBox
 
 
+def create_or_open_db(db_file):
+  conn = sqlite3.connect(db_file)
+
+  conn.execute(
+  """CREATE TABLE IF NOT EXISTS studies (
+	EGAS varchar(15) PRIMARY KEY,
+	ERP varchar(10),
+	PMID integer,
+	title text NOT NULL
+      );"""
+  )
+
+  return conn
+
+
 def extractStudyInfo(path):
-  """extracts the 'interesting' elements from an EGA-study XML representation.
+  """Extracts the 'interesting' elements from an EGA-study XML representation.
   
   Parameter path must be a pathlib Path to a study XML file.
   
@@ -27,7 +45,7 @@ def extractStudyInfo(path):
   """
 
   log.debug("processing file %s", path)
-  
+
   result = dict()
 
   # extract EGAS-number from filename, since it appears nowhere in the XML.
@@ -54,16 +72,33 @@ def extractStudyInfo(path):
   log.debug("result for %s: %s", egasNumber, result)
 
 
+def save_study_info(fields):
+  """Saves the extracted study information into the sqlite DB.
+  """
+
+  pass # TODO
+
+
+def process_studies(db_conn):
+  studiesDir = boxDir / 'studies'
+
+  for s in studiesDir.glob('EGAS*'):
+    result = extractStudyInfo(s)
+    save_study_info(result)
+    break  # DEBUG: finish after first study, that's enough while dev'ing
+
+
+
 def main():
   log.basicConfig( level=log.DEBUG, format='%(levelname)s: %(message)s' )
 
   boxDir = getEgaXmlDir('ega-box-433')
-  log.info("slurping XMLs from %s", boxDir)
+  db_file = boxDir / dbName
+  log.info("slurping XMLs from %s into %s", boxDir, db_file)
   
-  studiesDir = boxDir / 'studies'
-  for s in studiesDir.glob('EGAS*'):
-    extractStudyInfo(s)
-    break  # DEBUG: finish after first study, that's enough while dev'ing
+  db_conn = create_or_open_db(db_file)
+
+  process_studies(db_conn)
     
   return 0
 
